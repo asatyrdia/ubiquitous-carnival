6fa5241ad7d29649a6a24866cd78b24bc6c34df7
asatyrdia <atyrdiara@mail.com>
2025-01-10 23:27:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,41 +7,10 @@ const path = "./data.json";
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(2, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
-  const data = { date: date };
-  jsonfile.writeFile(path, data, () => {
-    simpleGit()
-      .pull("origin", "main")
-      .then(() => {
-        simpleGit()
-          .add([path])
-          .commit(date, { "--date": date })
-          .push("origin", "main", { "--force": true }, (err) => {
-            if (err) {
-              console.error("Push error:", err);
-            } else {
-              console.log("Push successful!");
-            }
-          });
-      })
-      .catch((err) => {
-        console.error("Pull error:", err);
-      });
-  });
-};
 
-let counter = 0;
 
 const makeCommits = (n) => {
   if (n === 0) return;
-  counter++;
-  console.log(`Commit #${counter}`);
   
   const x = random.int(0, 54);
   const y = random.int(0, 6);
